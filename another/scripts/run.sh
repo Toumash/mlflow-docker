6f2147074912df4fd133a35b48a50873658399b8
Tomasz Dłuski <mr.toumash@gmail.com>
2020-08-23 23:20:24 +0200

    removes old files

--- another/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mlflow server \
-    --file-store $FILE_STORE \
-    --default-artifact-root $ARTIFACT_STORE \
-    --host $SERVER_HOST \
-    --port $SERVER_PORT
--- another/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export MLFLOW_TRACKING_UI=http://localhost:5000
-mlflow run git@github.com:databricks/mlflow-example.git -P alpha=0.5 --no-conda
