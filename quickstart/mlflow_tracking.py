e1a260b9e30a773283b6cd9bef6054b9fb336bae
Feng <wang_feng@live.com>
2022-12-17 17:09:24 +0100

    fixed mlflow import error

--- quickstart/mlflow_tracking.py
+++ quickstart/mlflow_tracking.py
@@ -1,7 +1,7 @@
 import os
 from random import random, randint
-
-from mlflow import mlflow,log_metric, log_param, log_artifacts
+import mlflow
+from mlflow import log_metric, log_param, log_artifacts
 
 if __name__ == "__main__":
     with mlflow.start_run() as run:
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("Running mlflow_tracking.py")
 
         log_param("param1", randint(0, 100))
-        
+
         log_metric("foo", random())
         log_metric("foo", random() + 1)
         log_metric("foo", random() + 2)
@@ -20,3 +20,4 @@ if __name__ == "__main__":
             f.write("hello world!")
 
         log_artifacts("outputs")
+
